596454b39adbbf3409ce25c84837bc53ef27cf21
gulalaiahmadi <gulalaiahmadi1376@gmail.com>
2022-10-20 22:33:29 +0500

    Required changes added

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const mobileMenu = document.querySelector('.icon');
 const menuHeader = document.querySelector('.header');
+const menu = document.querySelector('.menu');
+const menuLink = document.querySelectorAll('.link');
 const toggleMenu = () => {
   menuHeader.classList.toggle('active');
 };
-mobileMenu.addEventListener('click', () => toggleMenu());
+mobileMenu.addEventListener('click', () => toggleMenu());  
+const displayNone = () => {
+  menuHeader.classList.remove('.active');
+};
+menuLink.addEventListener('click', () => displayNone());  
