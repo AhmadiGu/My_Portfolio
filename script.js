909a8633f301459f9fecb87e227cf295b0ab6955
obed300 <obamfo@st.ug.edu.gh>
2022-10-20 12:02:58 +0000

    Query select the classes to work on in js

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 const mobileMenu = document.querySelector('.icon');
 const menuHeader = document.querySelector('.header');
-const toggleMenu = () => {
-  menuHeader.classList.toggle('active');
-};
-mobileMenu.addEventListener('click', () => toggleMenu());
