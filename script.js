b49c40d066a0a6d956dc4a2a5ad1e0db37c174bf
obed300 <obamfo@st.ug.edu.gh>
2022-10-20 12:05:59 +0000

    Add function to js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 const mobileMenu = document.querySelector('.icon');
 const menuHeader = document.querySelector('.header');
+const toggleMenu = () => {
+  menuHeader.classList.toggle('active');
+};
+mobileMenu.addEventListener('click', () => toggleMenu());
